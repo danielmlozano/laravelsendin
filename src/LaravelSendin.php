98f50f0f3926c60060e050b7f04902711613977d
realdanielmlozano <daniel@inweb.mx>
2021-01-21 18:42:05 -0600

    creating a contact WIP

--- src/LaravelSendin.php
+++ src/LaravelSendin.php
@@ -3,6 +3,7 @@
 namespace Danielmlozano\LaravelSendin;
 
 use Danielmlozano\LaravelSendin\Exceptions\SBException;
+use SendinBlue\Client\Model\CreateContact;
 
 class LaravelSendin extends Api
 {
@@ -15,12 +16,30 @@ class LaravelSendin extends Api
      */
     public static function getAccount()
     {
-        return (new LaravelSendin)->accountApi()->getAccount();
+        try {
+            return (new LaravelSendin)->accountApi()->getAccount();
+        } catch (\Exception $e) {
+            throw new SBException($e->getMessage());
+        }
     }
 
+    /**
+     * Static accesor. Returns a getFolders model instance
+     *
+     * @param int $limit
+     * @param int $offset
+     * @param string $sort
+     * @static
+     * @access public
+     * @return \SendinBlue\Client\Model\GetFolders
+     */
     public static function getFolders(int $limit = 20, int $offset = 0, $sort = "desc")
     {
-        return (new LaravelSendin)->contactsApi()->getFolders($limit, $offset, $sort);
+        try {
+            return (new LaravelSendin)->contactsApi()->getFolders($limit, $offset, $sort);
+        } catch (\Exception $e) {
+            throw new SBException($e->getMessage());
+        }
     }
 
 
@@ -36,7 +55,11 @@ class LaravelSendin extends Api
      */
     public static function getLists(int $limit = 20, int $offset = 0, $sort = "desc")
     {
-        return (new LaravelSendin)->listsApi()->getLists($limit, $offset, $sort);
+        try {
+            return (new LaravelSendin)->listsApi()->getLists($limit, $offset, $sort);
+        } catch (\Exception $e) {
+            throw new SBException($e->getMessage());
+        }
     }
 
     /**
@@ -49,6 +72,34 @@ class LaravelSendin extends Api
      */
     public static function getList(int $listId)
     {
-        return (new LaravelSendin)->listsApi()->getList($listId);
+        try {
+            return (new LaravelSendin)->listsApi()->getList($listId);
+        } catch (\Exception $e) {
+            throw new SBException($e->getMessage());
+        }
+    }
+
+    /**
+     * Creates a new Contact and adds it to the given lists
+     *
+     * @param Array $data
+     * @static
+     * @access public
+     * @return void
+     */
+    public static function createContact(string $email, array $listsIds = [], array $attributes = [])
+    {
+        try {
+            $data = [
+                'email' => $email,
+                'listsIds' => $listsIds,
+                'attributes' => (object) $attributes,
+            ];
+            return (new LaravelSendin)->contactsApi()->createContact(
+                new CreateContact($data)
+            );
+        } catch (\Exception $e) {
+            throw new SBException($e->getMessage());
+        }
     }
 }
